2bcfefb822eb37aa4ef63b22e4d22c01e298c92e
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2021-10-23 14:43:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -14,14 +14,19 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 
 const initialMongo = async () => {
-//   const db = mongoose.connection;
+  //   const db = mongoose.connection;
   try {
     // db.once("open", () => console.log("connected to mongodb"));
     await mongoose
-      .connect(process.env.DATABASE_URI_LOCAL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(
+        process.env.NODE_ENV === "production"
+          ? process.env.DATABASE_URI
+          : process.env.DATABASE_URI_LOCAL,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
       .catch((err) => handell(err));
   } catch (err) {
     console.log(err);
