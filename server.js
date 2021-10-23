356be6a88bccfd346ed26dd2dab397cc540d0fae
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2021-10-23 14:44:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
-require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
